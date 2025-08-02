cd0138948cd51960dabb4686a1cefe3761b25db0
Reuben <reubenabrahams77@gmail.com>
2025-08-02 14:54:09 +0200

    Modify settings.py file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h$h0kx*d!huuy*f&zvr@g)rqghn=d&3f%%cevs^r692k-yn#0_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
